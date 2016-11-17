db0fb5d252c1e6450f4810d78bb0ea152f389a34
yassyskywalker <easternnovelty@gmail.com>
2016-11-18 07:43:29 +0900

    migrate comment add

--- uwsgi.sh
+++ uwsgi.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 chown uwsgi /static
 chown uwsgi /media
+su -m uwsgi -c "python /app/manage.py migrate"
 su -m uwsgi -c "python /app/manage.py collectstatic --noinput"
 su -m uwsgi -c "/usr/local/bin/uwsgi --socket :5000 --wsgi-file /app/myapp/wsgi.py --master --processes 4 --threads 2 --chdir /app"
